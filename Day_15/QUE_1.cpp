1df4c531c80c6e9e9968ef94a289dc5b868d01bc
Abhijit Kailas Gadhave <91112733+AbhiGadhave11@users.noreply.github.com>
2023-05-27 08:55:30 +0530

    Update QUE_1.cpp

--- Day_15/QUE_1.cpp
+++ Day_15/QUE_1.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    string reverseWords(string s) 
-    {
+    string reverseWords(string s) {
         int n = s.size();
-        vector<string>vobj;
-        for(int i=0;i<n;i++)
+        int start=0,end=n-1;
+        while(s[start]== ' ')
+            start++;
+        while(s[end] == ' ')
+            end--;
+        string ans = "";
+        int ostart = start;
+        string str = "";
+        for(int i=ostart;i<=end;i++)
         {
             if(s[i] == ' ')
-                continue;
-            else
             {
-                string str;
-                while(i<n && s[i] != ' ')
+                str = s.substr(start,i-start);
+                str += ' ';
+                str += ans;
+                ans = str;
+                while(s[i] == ' ')
                 {
-                    str = str + s[i];
                     i++;
                 }
-                vobj.push_back(str);
+                start = i;
+                i--;
             }
-            
         }
-        string ans;
-        reverse(vobj.begin(),vobj.end());
-        for(auto &it : vobj)
-        {
-            ans = ans + it;
-            ans = ans + ' ';
-        }
-        ans.pop_back();
+        str = s.substr(start,(end+1)-start);
+        str += ' ';
+        str += ans;
+        ans = str;
+        if(ans[ans.size()-1]==' ')
+            ans.pop_back();
         return ans;
     }
 };
