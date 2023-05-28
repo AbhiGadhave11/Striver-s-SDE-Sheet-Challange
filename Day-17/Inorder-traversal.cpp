1df12aa1c59d41e4d0237cd0eb23ef065f9d2a07
Abhijit Kailas Gadhave <91112733+AbhiGadhave11@users.noreply.github.com>
2023-05-28 13:38:26 +0530

    Create Inorder-traversal.cpp

---
+++ Day-17/Inorder-traversal.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    
+    void inorder(TreeNode *root,vector<int>&v)
+    {
+        if(root == NULL)
+            return ;
+        inorder(root->left,v);
+        v.push_back(root->val);
+        inorder(root->right,v);
+    }
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        vector<int>vobj;
+        inorder(root,vobj);
+        
+        return vobj;
+        
+    }
+    
+};
