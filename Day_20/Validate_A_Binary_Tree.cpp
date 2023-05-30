54c01b1670fdb04e35f9ec81ade72abab70e47dd
Abhijit Kailas Gadhave <91112733+AbhiGadhave11@users.noreply.github.com>
2023-05-30 19:52:04 +0530

    Create Validate_A_Binary_Tree.cpp

---
+++ Day_20/Validate_A_Binary_Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    bool fun(TreeNode *root,long long min,long long max)
+    {
+        if(root == NULL)
+            return true;
+        if(root->val<=min || root->val >= max)
+        {
+            return false;
+        }
+        bool left = fun(root->left,min,root->val);
+        bool right = fun(root->right,root->val,max);
+        if(left && right)
+            return true;
+        return false;
+    }
+public:
+    bool isValidBST(TreeNode* root) 
+    {
+        return fun(root,-1e10,1e10);
+        
+    }
+};
